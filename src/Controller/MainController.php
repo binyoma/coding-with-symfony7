08bab7147a73b5d7c64e8b5df3409bdc092aeae6
binyoma <innocentbinyoma@pm.me>
2025-03-26 16:06:05 +0100

    twig extensions

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -4,6 +4,7 @@ namespace App\Controller;
 
 use App\Repository\StarshipRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\DependencyInjection\Attribute\Autowire;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 use Symfony\Contracts\Cache\CacheInterface;
@@ -14,24 +15,15 @@ class MainController extends AbstractController
 {
     #[Route('/', name: 'app_homepage')]
     public function homepage(
-        StarshipRepository $starshipRepository,
-        HttpClientInterface $client,
-        CacheInterface $issLocationPool
+        StarshipRepository $starshipRepository
     ): Response
     {
         $ships = $starshipRepository->findAll();
         $myShip = $ships[array_rand($ships)];
 
-
-       $issData = $issLocationPool->get('iss_location_data',function () use ($client): array {
-           $response = $client->request('GET', 'https://api.wheretheiss.at/v1/satellites/25544');
-           return $response->toArray();
-       });
-
         return $this->render('main/homepage.html.twig', [
             'myShip' => $myShip,
-            'ships' => $ships,
-            'issData' => $issData,
+            'ships' => $ships
         ]);
     }
 }
