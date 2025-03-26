34e0e176321a0e254810dbba9167583ba2f4dd90
binyoma <innocentbinyoma@pm.me>
2025-03-26 09:34:56 +0100

    making the design dynamic

--- src/Model/Starship.php
+++ src/Model/Starship.php
@@ -9,7 +9,7 @@ class Starship
         private string $name,
         private string $class,
         private string $captain,
-        private string $status,
+        private StarshipStatusEnum $status,
     )
     {
     }
@@ -54,14 +54,24 @@ class Starship
         $this->captain = $captain;
     }
 
-    public function getStatus(): string
+    public function getStatus(): StarshipStatusEnum
     {
         return $this->status;
     }
-
-    public function setStatus(string $status): void
+    public function getStatusString(): string
     {
-        $this->status = $status;
+        return $this->status->value;
     }
+    public function getStatusImageFilename(): string
+    {
+        return match ($this->status) {
+            StarshipStatusEnum::WAITING => 'images/status-waiting.png',
+            StarshipStatusEnum::IN_PROGRESS => 'images/status-in-progress.png',
+            StarshipStatusEnum::COMPLETED => 'images/status-complete.png',
+        };
+
+    }
+
+
 
 }
