4891176effe5bb0480845f789d1b48fc08c01a5a
binyoma <innocentbinyoma@pm.me>
2025-03-27 08:44:39 +0100

    foundry & pagination

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -5,6 +5,7 @@ namespace App\Controller;
 
 use App\Repository\StarshipRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
@@ -14,9 +15,12 @@ class MainController extends AbstractController
     #[Route('/', name: 'app_homepage')]
     public function homepage(
        StarshipRepository $repository,
+        Request $request
     ): Response
     {
         $ships =$repository->findIncomplete();
+        $ships->setMaxPerPage(5);
+        $ships->setCurrentPage($request->query->get('page', 1));
         $myShip = $repository->findMyShip();
 
         return $this->render('main/homepage.html.twig', [
--- src/Repository/StarshipRepository.php
+++ src/Repository/StarshipRepository.php
@@ -6,6 +6,8 @@ use App\Entity\Starship;
 use App\Entity\StarshipStatusEnum;
 use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
 use Doctrine\Persistence\ManagerRegistry;
+use Pagerfanta\Doctrine\ORM\QueryAdapter;
+use Pagerfanta\Pagerfanta;
 
 /**
  * @extends ServiceEntityRepository<Starship>
@@ -25,13 +27,15 @@ class StarshipRepository extends ServiceEntityRepository
     /**
      * @return Starship[]
      */
-    public function findIncomplete(): array
+    public function findIncomplete(): Pagerfanta
     {
-        return $this->createQueryBuilder('e')
+        $query= $this->createQueryBuilder('e')
             ->where('e.status != :status')
+            ->orderBy('e.arrivedAt', 'DESC')
             ->setParameter('status', StarshipStatusEnum::COMPLETED)
-            ->getQuery()
-            ->getResult();
+            ->getQuery();
+
+        return new Pagerfanta(new QueryAdapter($query));
 
     }
 
